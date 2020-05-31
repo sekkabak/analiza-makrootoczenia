956c596cdb34e918485ce1a9bb5068b541b45fba
Cezary Bąk <sekkabak@gmail.com>
2020-05-31 16:44:03 +0200

    fix fillFactor

--- src/model/Factor.java
+++ src/model/Factor.java
@@ -1,11 +1,18 @@
 package model;
 
+import app.Config;
+
+import java.util.ArrayList;
+
 public class Factor {
     private String name;
-    public FactorPart[] rows = new FactorPart[3];
+    public ArrayList<FactorPart> rows = new ArrayList<>();
 
     public Factor(String name) {
         this.name = name;
+        for (int i = 0; i < 3; i++) {
+            rows.add(new FactorPart(Config.factorPartsName[i]));
+        }
     }
 
     public String getName() {
